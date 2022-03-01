9a768e7c44fde62512164afefcba8f7edd2e79ca
Vrushalikokare67 <86995729+Vrushalikokare67@users.noreply.github.com>
2022-03-01 16:35:27 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log('Example log listening at http://localhost:${port}')
+});
+
+app.get('/foo',function (req,res){
+    res.send('hello team iauro')
+});
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.post('/bar',function(req,res){
+    var body = req.body;
+    console.log(req.body.foo);
+    res.send(req.body.foo);
+});
